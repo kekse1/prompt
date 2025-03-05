58d0c63d6afeae242b532ff94ed7f1f7d5d16061
Sebastian Kucharczyk <kuchen@kekse.biz>
2025-03-05 20:14:02 +0100

    1741202040066454950

--- src/prompt.sh
+++ src/prompt.sh
@@ -32,7 +32,8 @@ _CODE=1
 _list()
 {
 	local _color="yes"; [[ $_ANSI -eq 0 ]] && _color="no"
-	echo; command ls -t --group-directories-first --color=${_color}
+	local data="$(command ls -t --group-directories-first --color=${_color})"
+	[[ ${#data} -gt 0 ]] && echo -e "\n${data}\n"
 }
 
 #
